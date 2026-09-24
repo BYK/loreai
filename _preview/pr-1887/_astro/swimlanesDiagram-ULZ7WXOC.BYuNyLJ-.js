1aded6acd87ebd78b18687e1b1f03e8c664e0b61
BYK <BYK@users.noreply.github.com>
2026-09-24 12:07:40 +0000

    Remove preview for PR 1887 🛬

--- _preview/pr-1887/_astro/swimlanesDiagram-ULZ7WXOC.BYuNyLJ-.js
+++
@@ -1,8 +0,0 @@
-import"./src.OgxTg4hg.js";import{n as e}from"./chunk-Y2CYZVJY.DsF7k-Jl.js";import"./chunk-I66GZJ75.DlV2AZBs.js";import"./chunk-NSK5VX7P.BzjaXQ-l.js";import"./chunk-ZIRB5QZD.Dh20_f-h.js";import"./chunk-W5SLKNZC.BSlx657f.js";import"./chunk-UBXNYLIW.KoqHeOSo.js";import"./chunk-4I5QYGJK.BGRr7nRm.js";import"./chunk-WRU74C26.DYCxb0Mm.js";import"./chunk-QR6OTTB3.DQvAeZjO.js";import"./chunk-7BUUIJ7U.CAhmNhj7.js";import"./chunk-7Z6QIM7H.DLMIKOF2.js";import"./chunk-J7OUQ5F2.BR0Jdi0K.js";import"./chunk-2GRJ4B5K.dTJmF95z.js";import"./chunk-XXDRQBXY.B0iYp7hA.js";import"./chunk-KBJHAD2P.BprstPNb.js";import{r as t,t as n}from"./chunk-JQJVKLGR.Dwf26_QK.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
-  .swimlane.cluster rect {
-    stroke: ${e.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,`getStyles`)});export{r as diagram};
